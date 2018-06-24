d710b6fc034cc1bf66c33666cc76c0c7b7f9347b
Léo <ungarscool1@users.noreply.github.com>
2018-06-24 16:56:58 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const express = require('express');
 const app = express()
+cont appName = 'YOUR_APP_NAME';
 const token = 'YOUR_OAUTH_TOKEN';
 const key = 'YOUR_API_KEY';
 
