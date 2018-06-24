737449888b3a23d9baa7ff2caf5afff0438599ec
Léo <ungarscool1@users.noreply.github.com>
2018-06-24 17:00:07 +0200

    problem at appName

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const express = require('express');
 const app = express()
-cont appName = 'YOUR_APP_NAME';
+const appName = 'YOUR_APP_NAME';
 const token = 'YOUR_OAUTH_TOKEN';
 const key = 'YOUR_API_KEY';
 
